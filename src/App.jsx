75d74e5f53c63f7d8b3a4fbb8e5085c64462d634
Hannah <hannahsenft@gmx.net>
2022-01-13 10:06:54 +0100

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import Button from "../src/components/Button";
 
+const Code = ({ code }) => (
+	<pre>
+		<code>{JSON.stringify(code, null, 4)}</code>
+	</pre>
+);
+// React internal hooks
+// useState
+// useEffect
+// useMemo
+// useRef
+// useContext
+// useReducer
+// use LayoutEffect
+// useCallBack
+// useImperativeHandle
+// useDebugValue
+
+const usePerson = () => {
+	return { name: "Gregor" };
+};
+
 const App = () => {
-  return (
-    <div>
-      <h1>Hello test</h1>
-      <Button>Click me!</Button>
-    </div>
-  );
+	const [state, setState] = useState("Gregor");
+	const handleClick = useCallback(() => {
+		console.log("Hello!");
+	}, []);
+	const { name } = usePerson();
+	return (
+		<div>
+			<Code code={name} />
+
+		</div>
+	);
 };
 
 export default App;
